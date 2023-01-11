6719339773928884eb97a24be52caeaa1a27e18d
lokmannhi@gmail.com <lokmannhi@gmail.com>
2023-01-11 16:53:19 +0800

    second test submit

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -3,11 +3,54 @@ import { useState, useEffect } from "react";
 import ReactLoading from "react-loading";
 import axios from "axios";
 import Modal from "react-modal";
+import { NavLink } from "react-router-dom";
 
 function PokeDex() {
   const [pokemons, setPokemons] = useState([]);
   const [pokemonDetail, setPokemonDetail] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    `https://pokeapi.co/api/v2/pokemon`
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [sortType, setSortType] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(currentPageUrl)
+      .then((response) => {
+        if (sortType === "name") {
+          const sorted = response.data.results.sort((a, b) =>
+            a.name.localeCompare(b.name)
+          );
+          setPokemons(sorted);
+          setIsLoading(false);
+        } else {
+          setIsLoading(false);
+          setPokemons(response.data.results);
+          setNextPageUrl(response.data.next);
+          setPrevPageUrl(response.data.previous);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [currentPageUrl, sortType]);
+
+  const handleSearch = (event) => {
+    let { value } = event.target;
+    setSearch(value);
+  };
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
 
   const customStyles = {
     content: {
@@ -62,14 +105,65 @@ function PokeDex() {
           <>
             <div className="App">
               <header className="App-header">
-                <b>Implement loader here</b>
+                <b>
+                  <ReactLoading />
+                </b>
               </header>
             </div>
           </>
         ) : (
           <>
-            <h1>Welcome to pokedex !</h1>
-            <b>Implement Pokedex list here</b>
+            <div className="container">
+              <h1>Welcome to pokedex !</h1>
+              <div className="search_filter">
+                <input
+                  type="text"
+                  value={search}
+                  placeholder="search...."
+                  onChange={handleSearch}
+                  className="search_bar"
+                />
+                <select
+                  value={pokemons.name}
+                  onChange={(e) => setSortType(e.target.value)}
+                >
+                  <option value="default">default</option>
+                  <option value="name">By Name (A-Z)</option>
+                </select>
+              </div>
+              {pokemons.length > 0 && (
+                <div>
+                  {pokemons
+                    .filter((item) =>
+                      item.name.toLowerCase().includes(search.toLowerCase())
+                    )
+                    .map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="card"
+                          onClick={() => setPokemonDetail(item)}
+                        >
+                          <NavLink
+                            to={`/pokedex/${item.name}`}
+                            style={{ textDecoration: "none" }}
+                          >
+                            <h6>{item.name}</h6>
+                          </NavLink>
+                        </div>
+                      );
+                    })}
+                </div>
+              )}
+              <div className="pagination_button">
+                <button type="btn" onClick={goToPrevPage}>
+                  Previous
+                </button>
+                <button type="btn" onClick={goToNextPage}>
+                  next
+                </button>
+              </div>
+            </div>
           </>
         )}
       </header>
